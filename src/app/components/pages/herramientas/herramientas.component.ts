1a01851f464b052eb5098be7c84017785b5b478f
Rodrigo <rodrigopalominocastro0@gmail.com>
2025-01-06 00:32:36 -0500

    ..

--- src/app/components/pages/herramientas/herramientas.component.ts
+++ src/app/components/pages/herramientas/herramientas.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './herramientas.component.html',
-  styleUrl: './herramientas.component.css'
+  styleUrl: './herramientas.component.css',
 })
 export class HerramientasComponent {
-
+  herramientas: any = [
+    { img: '../../../assets/img/react.svg', name: 'React' },
+    { img: '../../../assets/img/angular.svg', name: 'Angular' },
+    { img: '../../../assets/img/node.svg', name: 'NodeJs' },
+    { img: '../../../assets/img/nest.svg', name: 'Nest' },
+    { img: '../../../assets/img/next.svg', name: 'Next' },
+    { img: '../../../assets/img/django.svg', name: 'Django' },
+    { img: '../../../assets/img/flask.svg', name: 'Flask' },
+    { img: '../../../assets/img/sqlserver.svg', name: 'SqlServer' },
+    { img: '../../../assets/img/mysql.svg', name: 'MySql' },
+    { img: '../../../assets/img/mongoDB.svg', name: 'MongoDB' },
+    { img: '../../../assets/img/github.svg', name: 'Github' },
+    { img: '../../../assets/img/git.png', name: 'Git' },
+    { img: '../../../assets/img/figma.svg', name: 'Figma' },
+  ];
 }
